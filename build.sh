259a0e1c3fefceb596414beb1fa5149dceddd738
khai96_ <hvksmr1996@gmail.com>
2016-09-05 15:33:44 +0700

    I need more information

--- build.sh
+++ build.sh
@@ -7,13 +7,12 @@ curl -XGET $GCC_SRC_URL > "$GCC_FSNAME.tar.gz"
 echo "Decompressing 'source.tar.gz'..."
 tar -xzf "$GCC_FSNAME.tar.gz"
 
+export REPO_DIR=$(pwd)
 export SOURCE_DIR=$(pwd)/$GCC_FSNAME
 export OBJECT_DIR=$(pwd)/object
 mkdir $OBJECT_DIR
 
 cd $SOURCE_DIR
-ls -a $SOURCE_DIR
-
 echo 'Downloading prerequisites...'
 ./contrib/download_prerequisites
 
@@ -21,6 +20,13 @@ cd $OBJECT_DIR
 
 echo 'Configuring...'
 $SOURCE_DIR/configure --prefix=$OBJECT_DIR/out --enable-languages=c,c++
+
+echo "Contents of '$SOURCE_DIR'"
+ls -a $SOURCE_DIR
+echo "Contents of '$OBJECT_DIR'"
+ls -a $OBJECT_DIR
+echo "Contents of '$REPO_DIR'"
+ls -a $REPO_DIR
 
 echo 'Building...'
 make
