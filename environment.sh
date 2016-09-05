973ac9f490108d21d6bbf4c77d7f89e8d7e965e8
khai96_ <hvksmr1996@gmail.com>
2016-09-05 15:56:26 +0700

    update environment

--- environment.sh
+++ environment.sh
@@ -2,3 +2,4 @@ export GCC_VERSION=$(./version.sh)
 export GCC_MIRROR=$(./mirror.sh)
 export GCC_FSNAME="gcc-$GCC_VERSION"
 export GCC_SRC_URL="$GCC_MIRROR/gcc/releases/$GCC_FSNAME/$GCC_FSNAME.tar.gz"
+export LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LIBRARY_PATH
