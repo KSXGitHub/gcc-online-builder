543bd4afbac923429d408e30700e7ae125a88d47
khai96_ <hvksmr1996@gmail.com>
2016-09-06 15:32:40 +0700

    add logic to /all-scripts.sh

--- all-scripts.sh
+++ all-scripts.sh
@@ -1,6 +1,8 @@
 
 # Use this for non-ci environment
 
-./download.sh
-./prerequisites.sh
-./build.sh
+./download.sh && ./prerequisites.sh && ./build.sh || {
+    exitcode=$?
+    echo 'FAILED' >&2
+    exit exitcode
+}
