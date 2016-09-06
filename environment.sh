920653959b75a3f082fd454b4bc2063b55e670f9
khai96_ <hvksmr1996@gmail.com>
2016-09-06 14:45:33 +0700

    add more environment variables

--- environment.sh
+++ environment.sh
@@ -1,8 +1,10 @@
 export GCC_VERSION=$(./version.sh)
 export GCC_MIRROR=$(./mirror.sh)
 export GCC_FSNAME="gcc-$GCC_VERSION"
-export GCC_SRC_URL="$GCC_MIRROR/gcc/releases/$GCC_FSNAME/$GCC_FSNAME.tar.gz"
+export GCC_ARCHIVE="$GCC_FSNAME.tar.gz"
+export GCC_SRC_URL="$GCC_MIRROR/gcc/releases/$GCC_FSNAME/$GCC_ARCHIVE"
 export REPO_DIR=$(pwd)
+export GCC_SRC_ARC=$REPO_DIR/$GCC_ARCHIVE
 export SOURCE_DIR=$(pwd)/$GCC_FSNAME
 export OBJECT_DIR=$(pwd)/object
 export LOGS_DIR=$(pwd)/logs
