0ad0c01cdb8b091f522f95f488e501997f79c806
khai96_ <hvksmr1996@gmail.com>
2016-09-06 15:33:35 +0700

    prompt if success

--- all-scripts.sh
+++ all-scripts.sh
@@ -1,7 +1,9 @@
 
 # Use this for non-ci environment
 
-./download.sh && ./prerequisites.sh && ./build.sh || {
+./download.sh && ./prerequisites.sh && ./build.sh && {
+    echo 'SUCCESS'
+} || {
     exitcode=$?
     echo 'FAILED' >&2
     exit exitcode
